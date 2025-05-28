34e9039fe66b97f4d82b5ef79853b95ab3061ffa
Paul Barrett <pebarrett@gmail.com>
2025-05-28 10:25:58 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(;
     warnonly = [:missing_docs],
     pages = [
         "Home" => "index.md",
-        "API" => "api.md",        
+        "API" => "api.md",
         "HDU" => Any[
             "Introduction" => "HDU.md",
             "Primary" => "HDU/primary.md",
